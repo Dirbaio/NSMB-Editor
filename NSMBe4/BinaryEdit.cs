8a9bffdc6980fdd4dd85e620ac0e93ec9d22b20e
yosoydario <yosoydario@acd78428-90d6-11de-b045-6380ec9940d4>
2011-08-04 12:54:33 +0000

    Testing stuff.

--- NSMBe4/BinaryEdit.cs
+++ NSMBe4/BinaryEdit.cs
@@ -14,7 +14,7 @@
 *   You should have received a copy of the GNU General Public License
 *   along with NSMB Editor 5.  If not, see <http://www.gnu.org/licenses/>.
 */
-
+ 
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
