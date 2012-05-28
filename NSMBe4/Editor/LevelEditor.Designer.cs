dd9b680089ec139c3262707342cbeaef73509cab
Piranhaplant <piranhaplant1@gmail.com>
2012-05-28 13:21:10 -0500

    Removed level config button from toolbar.

--- NSMBe4/Editor/LevelEditor.Designer.cs
+++ NSMBe4/Editor/LevelEditor.Designer.cs
@@ -27,7 +27,6 @@
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.saveLevelButton = new System.Windows.Forms.ToolStripButton();
             this.viewMinimapButton = new System.Windows.Forms.ToolStripButton();
-            this.levelConfigButton = new System.Windows.Forms.ToolStripButton();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.cutButton = new System.Windows.Forms.ToolStripButton();
             this.copyButton = new System.Windows.Forms.ToolStripButton();
@@ -91,7 +90,6 @@
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.saveLevelButton,
             this.viewMinimapButton,
-            this.levelConfigButton,
             this.toolStripSeparator1,
             this.cutButton,
             this.copyButton,
@@ -111,7 +109,7 @@
             this.toolStripDropDownButton1});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(1370, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(1028, 25);
             this.toolStrip1.TabIndex = 5;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -134,16 +132,6 @@
             this.viewMinimapButton.Size = new System.Drawing.Size(23, 22);
             this.viewMinimapButton.Text = "<viewMinimapButton>";
             this.viewMinimapButton.Click += new System.EventHandler(this.viewMinimapButton_Click);
-            // 
-            // levelConfigButton
-            // 
-            this.levelConfigButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.levelConfigButton.Image = ((System.Drawing.Image)(resources.GetObject("levelConfigButton.Image")));
-            this.levelConfigButton.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.levelConfigButton.Name = "levelConfigButton";
-            this.levelConfigButton.Size = new System.Drawing.Size(23, 22);
-            this.levelConfigButton.Text = "<levelConfigButton>";
-            this.levelConfigButton.Click += new System.EventHandler(this.levelConfigButton_Click);
             // 
             // toolStripSeparator1
             // 
@@ -275,31 +263,31 @@
             // toolStripMenuItem13
             // 
             this.toolStripMenuItem13.Name = "toolStripMenuItem13";
-            this.toolStripMenuItem13.Size = new System.Drawing.Size(135, 22);
+            this.toolStripMenuItem13.Size = new System.Drawing.Size(105, 22);
             this.toolStripMenuItem13.Text = "800 %";
             // 
             // toolStripMenuItem12
             // 
             this.toolStripMenuItem12.Name = "toolStripMenuItem12";
-            this.toolStripMenuItem12.Size = new System.Drawing.Size(135, 22);
+            this.toolStripMenuItem12.Size = new System.Drawing.Size(105, 22);
             this.toolStripMenuItem12.Text = "400 %";
             // 
             // toolStripMenuItem11
             // 
             this.toolStripMenuItem11.Name = "toolStripMenuItem11";
-            this.toolStripMenuItem11.Size = new System.Drawing.Size(135, 22);
+            this.toolStripMenuItem11.Size = new System.Drawing.Size(105, 22);
             this.toolStripMenuItem11.Text = "200 %";
             // 
             // toolStripMenuItem10
             // 
             this.toolStripMenuItem10.Name = "toolStripMenuItem10";
-            this.toolStripMenuItem10.Size = new System.Drawing.Size(135, 22);
+            this.toolStripMenuItem10.Size = new System.Drawing.Size(105, 22);
             this.toolStripMenuItem10.Text = "150 %";
             // 
             // toolStripMenuItem9
             // 
             this.toolStripMenuItem9.Name = "toolStripMenuItem9";
-            this.toolStripMenuItem9.Size = new System.Drawing.Size(135, 22);
+            this.toolStripMenuItem9.Size = new System.Drawing.Size(105, 22);
             this.toolStripMenuItem9.Text = "125 %";
             // 
             // toolStripMenuItem2
@@ -307,43 +295,43 @@
             this.toolStripMenuItem2.Checked = true;
             this.toolStripMenuItem2.CheckState = System.Windows.Forms.CheckState.Checked;
             this.toolStripMenuItem2.Name = "toolStripMenuItem2";
-            this.toolStripMenuItem2.Size = new System.Drawing.Size(135, 22);
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(105, 22);
             this.toolStripMenuItem2.Text = "100 %";
             // 
             // toolStripMenuItem1
             // 
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(135, 22);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(105, 22);
             this.toolStripMenuItem1.Text = "85 %";
             // 
             // toolStripMenuItem7
             // 
             this.toolStripMenuItem7.Name = "toolStripMenuItem7";
-            this.toolStripMenuItem7.Size = new System.Drawing.Size(135, 22);
+            this.toolStripMenuItem7.Size = new System.Drawing.Size(105, 22);
             this.toolStripMenuItem7.Text = "75 %";
             // 
             // toolStripMenuItem3
             // 
             this.toolStripMenuItem3.Name = "toolStripMenuItem3";
-            this.toolStripMenuItem3.Size = new System.Drawing.Size(135, 22);
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(105, 22);
             this.toolStripMenuItem3.Text = "66 %";
             // 
             // toolStripMenuItem4
             // 
             this.toolStripMenuItem4.Name = "toolStripMenuItem4";
-            this.toolStripMenuItem4.Size = new System.Drawing.Size(135, 22);
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(105, 22);
             this.toolStripMenuItem4.Text = "50 %";
             // 
             // toolStripMenuItem5
             // 
             this.toolStripMenuItem5.Name = "toolStripMenuItem5";
-            this.toolStripMenuItem5.Size = new System.Drawing.Size(135, 22);
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(105, 22);
             this.toolStripMenuItem5.Text = "33 %";
             // 
             // toolStripMenuItem6
             // 
             this.toolStripMenuItem6.Name = "toolStripMenuItem6";
-            this.toolStripMenuItem6.Size = new System.Drawing.Size(135, 22);
+            this.toolStripMenuItem6.Size = new System.Drawing.Size(105, 22);
             this.toolStripMenuItem6.Text = "25 %";
             // 
             // toolStripSeparator3
@@ -376,14 +364,14 @@
             // reloadTilesets
             // 
             this.reloadTilesets.Name = "reloadTilesets";
-            this.reloadTilesets.Size = new System.Drawing.Size(359, 22);
+            this.reloadTilesets.Size = new System.Drawing.Size(295, 22);
             this.reloadTilesets.Text = "<reloadTilesets>";
             this.reloadTilesets.Click += new System.EventHandler(this.reloadTilesets_Click);
             // 
             // smallBlockOverlaysToolStripMenuItem
             // 
             this.smallBlockOverlaysToolStripMenuItem.Name = "smallBlockOverlaysToolStripMenuItem";
-            this.smallBlockOverlaysToolStripMenuItem.Size = new System.Drawing.Size(359, 22);
+            this.smallBlockOverlaysToolStripMenuItem.Size = new System.Drawing.Size(295, 22);
             this.smallBlockOverlaysToolStripMenuItem.Text = "<smallBlockOverlaysToolStripMenuItem>";
             this.smallBlockOverlaysToolStripMenuItem.Click += new System.EventHandler(this.smallBlockOverlaysToolStripMenuItem_Click);
             // 
@@ -404,7 +392,7 @@
             // 
             this.setBgImageButton.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.setBgImageButton.Name = "setBgImageButton";
-            this.setBgImageButton.Size = new System.Drawing.Size(169, 22);
+            this.setBgImageButton.Size = new System.Drawing.Size(135, 22);
             this.setBgImageButton.Text = "Set BG";
             this.setBgImageButton.Click += new System.EventHandler(this.setBgImageButton_Click);
             // 
@@ -412,14 +400,14 @@
             // 
             this.removeBgButton.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.removeBgButton.Name = "removeBgButton";
-            this.removeBgButton.Size = new System.Drawing.Size(169, 22);
+            this.removeBgButton.Size = new System.Drawing.Size(135, 22);
             this.removeBgButton.Text = "Remove BG";
             this.removeBgButton.Click += new System.EventHandler(this.removeBgButton_Click);
             // 
             // moveBGToolStripMenuItem
             // 
             this.moveBGToolStripMenuItem.Name = "moveBGToolStripMenuItem";
-            this.moveBGToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.moveBGToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
             this.moveBGToolStripMenuItem.Text = "Move BG";
             this.moveBGToolStripMenuItem.Click += new System.EventHandler(this.moveBGToolStripMenuItem_Click);
             // 
@@ -432,7 +420,6 @@
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
             this.splitContainer1.Location = new System.Drawing.Point(0, 25);
-            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -442,9 +429,8 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.levelEditorControl1);
-            this.splitContainer1.Size = new System.Drawing.Size(1370, 684);
+            this.splitContainer1.Size = new System.Drawing.Size(1028, 551);
             this.splitContainer1.SplitterDistance = 249;
-            this.splitContainer1.SplitterWidth = 5;
             this.splitContainer1.TabIndex = 12;
             this.splitContainer1.SplitterMoved += new System.Windows.Forms.SplitterEventHandler(this.splitContainer1_SplitterMoved);
             // 
@@ -453,7 +439,6 @@
             this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer2.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
             this.splitContainer2.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.splitContainer2.Name = "splitContainer2";
             this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
@@ -464,9 +449,8 @@
             // splitContainer2.Panel2
             // 
             this.splitContainer2.Panel2.Controls.Add(this.tabControl1);
-            this.splitContainer2.Size = new System.Drawing.Size(249, 684);
-            this.splitContainer2.SplitterDistance = 567;
-            this.splitContainer2.SplitterWidth = 5;
+            this.splitContainer2.Size = new System.Drawing.Size(249, 551);
+            this.splitContainer2.SplitterDistance = 434;
             this.splitContainer2.TabIndex = 4;
             // 
             // panel1
@@ -474,18 +458,16 @@
             this.panel1.Controls.Add(this.PanelContainer);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(249, 567);
+            this.panel1.Size = new System.Drawing.Size(249, 434);
             this.panel1.TabIndex = 11;
             // 
             // PanelContainer
             // 
             this.PanelContainer.Dock = System.Windows.Forms.DockStyle.Fill;
             this.PanelContainer.Location = new System.Drawing.Point(0, 0);
-            this.PanelContainer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.PanelContainer.Name = "PanelContainer";
-            this.PanelContainer.Size = new System.Drawing.Size(249, 567);
+            this.PanelContainer.Size = new System.Drawing.Size(249, 434);
             this.PanelContainer.TabIndex = 10;
             // 
             // tabControl1
@@ -495,21 +477,19 @@
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
-            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabControl1.Multiline = true;
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(249, 112);
+            this.tabControl1.Size = new System.Drawing.Size(249, 113);
             this.tabControl1.TabIndex = 1;
             // 
             // tabPage1
             // 
             this.tabPage1.Controls.Add(this.minimapControl1);
             this.tabPage1.Location = new System.Drawing.Point(4, 4);
-            this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.tabPage1.Size = new System.Drawing.Size(241, 83);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage1.Size = new System.Drawing.Size(241, 87);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Minimap";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -517,20 +497,19 @@
             // minimapControl1
             // 
             this.minimapControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.minimapControl1.Location = new System.Drawing.Point(4, 4);
-            this.minimapControl1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.minimapControl1.Location = new System.Drawing.Point(3, 3);
+            this.minimapControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.minimapControl1.Name = "minimapControl1";
-            this.minimapControl1.Size = new System.Drawing.Size(233, 75);
+            this.minimapControl1.Size = new System.Drawing.Size(235, 81);
             this.minimapControl1.TabIndex = 0;
             // 
             // tabPage2
             // 
             this.tabPage2.Controls.Add(this.coordinateViewer1);
             this.tabPage2.Location = new System.Drawing.Point(4, 4);
-            this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.tabPage2.Size = new System.Drawing.Size(324, 109);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage2.Size = new System.Drawing.Size(179, 66);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Obj Position";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -539,10 +518,10 @@
             // 
             this.coordinateViewer1.AutoSize = true;
             this.coordinateViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.coordinateViewer1.Location = new System.Drawing.Point(4, 4);
-            this.coordinateViewer1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.coordinateViewer1.Location = new System.Drawing.Point(3, 3);
+            this.coordinateViewer1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.coordinateViewer1.Name = "coordinateViewer1";
-            this.coordinateViewer1.Size = new System.Drawing.Size(313, 95);
+            this.coordinateViewer1.Size = new System.Drawing.Size(175, 63);
             this.coordinateViewer1.TabIndex = 0;
             // 
             // levelEditorControl1
@@ -551,21 +530,20 @@
             this.levelEditorControl1.BackColor = System.Drawing.SystemColors.Control;
             this.levelEditorControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.levelEditorControl1.Location = new System.Drawing.Point(0, 0);
-            this.levelEditorControl1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.levelEditorControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.levelEditorControl1.Name = "levelEditorControl1";
-            this.levelEditorControl1.Size = new System.Drawing.Size(1116, 684);
+            this.levelEditorControl1.Size = new System.Drawing.Size(775, 551);
             this.levelEditorControl1.TabIndex = 3;
             // 
             // LevelEditor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1370, 709);
+            this.ClientSize = new System.Drawing.Size(1028, 576);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.toolStrip1);
             this.DoubleBuffered = true;
             this.KeyPreview = true;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "LevelEditor";
             this.Text = "<_TITLE>";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.LevelEditor_FormClosing);
@@ -597,7 +575,6 @@
         private System.Windows.Forms.ToolStripButton viewMinimapButton;
         private System.Windows.Forms.ToolStripMenuItem smallBlockOverlaysToolStripMenuItem;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
-        private System.Windows.Forms.ToolStripButton levelConfigButton;
         private System.Windows.Forms.Panel PanelContainer;
         private System.Windows.Forms.ToolStripButton spriteFinder;
         private System.Windows.Forms.ToolStripDropDownButton zoomMenu;
--- NSMBe4/Editor/LevelEditor.cs
+++ NSMBe4/Editor/LevelEditor.cs
@@ -148,25 +148,11 @@ namespace NSMBe4 {
             MinimapForm.Show();
         }
 
-        private void levelConfigButton_Click(object sender, EventArgs e) {
-            if (LevelConfigForm == null || LevelConfigForm.IsDisposed) {
-                LevelConfigForm = new LevelConfig(levelEditorControl1);
-            }
-            LevelConfigForm.ReloadTileset += new LevelConfig.ReloadTilesetDelegate(LevelConfigForm_ReloadTileset);
-            LevelConfigForm.RefreshMainWindow += new LevelConfig.RefreshMainWindowDelegate(LevelConfigForm_RefreshMainWindow);
-            LevelConfigForm.LoadSettings();
-            LevelConfigForm.Show();
-        }
-
         public void LevelConfigForm_ReloadTileset() {
             GFX.LoadTilesets(Level.Blocks[0][0xC], Level.Blocks[2][2]);
             Level.ReRenderAll();
 
             oem.ReloadObjectPicker();
-            Invalidate(true);
-        }
-
-        private void LevelConfigForm_RefreshMainWindow() {
             Invalidate(true);
         }
 
