604549191da0c1ae4c2fb96bb75656c64ab4de86
piranhaplant <piranhaplant1@gmail.com>
2012-08-28 21:54:07 -0500

    Changed the tileset behavior editor to a list box.

--- NSMBe4/TilesetEditor/TileBehavior.cs
+++ NSMBe4/TilesetEditor/TileBehavior.cs
@@ -6,11 +6,17 @@ namespace NSMBe4
 {
     public class TileBehavior
     {
-        byte[] tb;
+        public byte[] tb;
         String name;
+        public bool isHeader;
 
         public TileBehavior(String tb, String name)
         {
+            this.name = name;
+            if (tb == string.Empty) {
+                isHeader = true;
+                return;
+            }
             String[] tb_string = tb.Split(' ');
             byte[] tb_byte = new byte[4];
             for (int count = 0; count < 4; count++)
@@ -18,17 +24,36 @@ namespace NSMBe4
                 tb_byte[count] = byte.Parse(tb_string[count], System.Globalization.NumberStyles.HexNumber);
             }
             this.tb = tb_byte;
-            this.name = name;
-        }
-
-        public byte[] getTb()
-        {
-            return tb;
         }
 
         public override String ToString()
         {
             return name;
+        }
+
+        public static List<TileBehavior> readFromFile(string fileName)
+        {
+            List<TileBehavior> behaviors = new List<TileBehavior>();
+            try
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader(fileName);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    if (line != string.Empty) {
+                        if (line.StartsWith("+"))
+                            behaviors.Add(new TileBehavior("", line.Substring(1)));
+                        else {
+                            int equalpos = line.IndexOf('=');
+                            behaviors.Add(new TileBehavior(line.Substring(0, equalpos - 1).Trim(), line.Substring(equalpos + 1).Trim()));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return behaviors;
         }
     }
 }
